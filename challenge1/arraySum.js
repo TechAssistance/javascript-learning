b67cd0b5af43eb2cdc97a2aa60111d416fe85951
Nicholas <76240748+TechAssistance@users.noreply.github.com>
2023-05-22 00:44:13 -0700

    Update arraySum.js
    added a way todo this w/o using the reduce function

--- challenge1/arraySum.js
+++ challenge1/arraySum.js
@@ -3,5 +3,16 @@ function calcSum(arr){
     return arr.reduce((callbackAccumulatedValue,currentValue) => callbackAccumulatedValue+currentValue, 0);
 }
 
+function calcSum2(arr){
+    //get sum of array w/o reduce
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+    
+
 arrayboi = [1,2,3,4,5,6,7,8,9,10];
 console.log(calcSum(arrayboi));
+console.log(calcSum2(arrayboi));
