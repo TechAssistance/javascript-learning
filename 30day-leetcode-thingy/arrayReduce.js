d7a87e21111bad630f37767e692c3771c2f3c45a
Nicholas <76240748+TechAssistance@users.noreply.github.com>
2023-05-28 05:30:43 -0700

    Update arrayReduce.js
    added my final attempt which was keeping the cache idea and the const declaration but try to stitch back to function declaration instead of notation.

--- 30day-leetcode-thingy/arrayReduce.js
+++ 30day-leetcode-thingy/arrayReduce.js
@@ -14,15 +14,24 @@ var reduce = function(nums, fn, init) {
 // cache the array length to avoid rechecking length on each iteration
 // declare using const to avoid reassigning (const oddly runs faster than var and let)
 // tryout function syntax
-
+// [57ms but 41MB]
 const reduce2 = (nums, fn, init) => {
     const length = nums.length;
-    if (length === 0) {
-        return init;
-    }
+    if (length === 0) return init;
     let accum = init;
     for (let i = 0; i < length; i++) {
         accum = fn(accum, nums[i]);
     }
     return accum;
 };
+
+// switch back to function declaration syntax (this gave fastest results) [53ms but 42MB]
+const reduce3 = function(nums, fn, init) {
+    const length = nums.length;
+    if (length === 0) return init;
+    let accum = init;
+    for (let i = 0; i < length; i++) {
+        accum = fn(accum, nums[i]);
+    }
+    return accum;
+}
